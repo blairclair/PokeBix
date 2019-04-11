99c27a5e8cbdc4bf1d0685dadb24ba30728f44e3
Scott Snelgrove <ssnelgro@e1z4r13p7.42.us.org>
2019-04-11 15:10:27 -0700

    A few changes to directory structure and I added findTypeRelations

---
+++ namespace.pokebix/code/findTypeByName.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var console = require('console')
+var config = require('config')
+
+module.exports.function = function findTypeByName (Type_name) {
+  var num = 1 + Math.floor(Math.random() * 807);
+  var response = http.getUrl('https://pokeapi.co/api/v2/type/' + Type_name, { format: 'json' });
+  console.log(response);
+  return response;
+}
---
+++ namespace.pokebix/code/findTypeRelations.js
@@ -0,0 +1,51 @@
+const type_effect_enum = {
+  DOUBLE_DAMAGE_TO: 'double damage to',
+  DOUBLE_DAMAGE_FROM: 'double damage from',
+  HALF_DAMAGE_TO: 'half damage to',
+  HALF_DAMAGE_FROM: 'half damage from',
+  NO_DAMAGE_TO: 'no damage to',
+  NO_DAMAGE_FROM: 'no damage from'
+}
+
+module.exports.function = function findTypeRelations(Type, Type_effect) {
+  var names = new Array(3);
+  var count = 0
+    switch (Type_effect) {
+      case type_effect_enum.DOUBLE_DAMAGE_TO:
+        for (var x in Type.damage_relations.double_damage_to) {
+          names[count] = Type.damage_relations.double_damage_to[x].name
+          count++
+        }
+      break;
+      case type_effect_enum.DOUBLE_DAMAGE_FROM:
+        for (var x in Type.damage_relations.double_damage_from) {
+          names[count] = Type.damage_relations.double_damage_from[x].name
+          count++
+        }
+      break;
+      case type_effect_enum.HALF_DAMAGE_TO:
+        for (var x in Type.damage_relations.half_damage_to) {
+          names[count] = Type.damage_relations.half_damage_to[x].name
+          count++
+        }
+      break;
+      case type_effect_enum.HALF_DAMAGE_FROM:
+          for (var x in Type.damage_relations.half_damage_from) {
+          names[count] = Type.damage_relations.half_damage_from[x].name
+          count++
+        }
+      break;
+      case type_effect_enum.NO_DAMAGE_TO:
+        for (var x in Type.damage_relations.no_damage_to) {
+          names[count] = Type.damage_relations.no_damage_to[x].name
+          count++
+        }
+      break;
+      case type_effect_enum.NO_DAMAGE_FROM:
+        for (var x in Type.damage_relations.no_damage_from) {
+          names[count] = Type.damage_relations.no_damage_from[x].name
+          count++
+        }
+    }
+    return (names)
+}
