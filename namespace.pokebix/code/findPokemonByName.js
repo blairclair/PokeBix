dd63a359a2a2337757135af43ef13ebc72d62fbb
Ariella Grodzinsky <agrodzin@e1z4r13p11.42.us.org>
2019-04-11 14:56:26 -0700

    added flavor text

--- namespace.pokebix/code/findPokemonByName.js
+++ namespace.pokebix/code/findPokemonByName.js
@@ -3,7 +3,18 @@ var console = require('console')
 var config = require('config')
 
 module.exports.function = function findPokemonByName (Pokemon_name) {
-  var response = http.getUrl('https://pokeapi.co/api/v2/pokemon/' + Pokemon_name, { format: 'json' });
-  console.log(response);
-  return response;
+  var pokemon = http.getUrl('https://pokeapi.co/api/v2/pokemon/' + Pokemon_name, { format: 'json' });
+  var species = http.getUrl('https://pokeapi.co/api/v2/pokemon-species/' + Pokemon_name, {format: 'json'})
+  var flavor = species.flavor_text_entries
+  
+  for (var i in flavor){
+    if (flavor[i].language.name == "en" && flavor[i].version.name == "red"){
+      pokemon.flavor_text = flavor[i].flavor_text
+    }
+    i++;
+  }
+  pokemon.extra = species
+  console.log(species.version);
+  // console.log(pokemon);
+  return pokemon;
 }
