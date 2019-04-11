95faba6deceadcfaa82210f163ae495ae816146a
Scott Snelgrove <ssnelgro@e1z4r13p7.42.us.org>
2019-04-10 17:52:15 -0700

    This should be up to date

--- namespace.pokebix/code/findPokemonbyName.js
+++ namespace.pokebix/code/findPokemonbyName.js
@@ -1,3 +1,9 @@
-module.exports.function = function findPokemonbyName (name) {
-  return {}
+var http = require('http')
+var console = require('console')
+var config = require('config')
+
+module.exports.function = function findPokemonbyName (Pokemon_name) {
+  var response = http.getUrl('https://pokeapi.co/api/v2/pokemon/' + Pokemon_name, { format: 'json' });
+  console.log(response);
+  return response;
 }
