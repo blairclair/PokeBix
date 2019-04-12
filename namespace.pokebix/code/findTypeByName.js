b2d0d4da82e32dae0a14331b6d2f87037b130c81
Collin Brill <collin.brill@gmail.com>
2019-04-11 19:32:26 -0700

    type fixes

--- namespace.pokebix/code/findTypeByName.js
+++ namespace.pokebix/code/findTypeByName.js
@@ -3,7 +3,6 @@ var console = require('console')
 var config = require('config')
 
 module.exports.function = function findTypeByName (Type_name) {
-  var num = 1 + Math.floor(Math.random() * 807);
   var response = http.getUrl('https://pokeapi.co/api/v2/type/' + Type_name, { format: 'json' });
   console.log(response);
   return response;
