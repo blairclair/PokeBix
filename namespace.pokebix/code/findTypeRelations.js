8a9b3d88c3ae66ccbe77452616e29f70edab791f
Collin Brill <cbrill@e1z4r13p7.42.us.org>
2019-04-12 15:50:03 -0700

    dynamic keys from not-enum

--- namespace.pokebix/code/findTypeRelations.js
+++ namespace.pokebix/code/findTypeRelations.js
@@ -1,53 +1,20 @@
 const type_effect_enum = {
-  DOUBLE_DAMAGE_TO: 'double damage to',
-  DOUBLE_DAMAGE_FROM: 'double damage from',
-  HALF_DAMAGE_TO: 'half damage to',
-  HALF_DAMAGE_FROM: 'half damage from',
-  NO_DAMAGE_TO: 'no damage to',
-  NO_DAMAGE_FROM: 'no damage from'
+  DOUBLE_DAMAGE_TO: 'double_damage_to',
+  DOUBLE_DAMAGE_FROM: 'double_damage_from',
+  HALF_DAMAGE_TO: 'half_damage_to',
+  HALF_DAMAGE_FROM: 'half_damage_from',
+  NO_DAMAGE_TO: 'no_damage_to',
+  NO_DAMAGE_FROM: 'no_damage_from'
 }
 
 
 module.exports.function = function findTypeRelations(Type, Type_effect) {
-  var names = new Array(3);
-  var count = 0
-    switch (Type_effect) {
-      case type_effect_enum.DOUBLE_DAMAGE_TO:
-        for (var x in Type.damage_relations.double_damage_to) {
-          names[count] = Type.damage_relations.double_damage_to[x].name
-          count++
-        }
-      break;
-      case type_effect_enum.DOUBLE_DAMAGE_FROM:
-        for (var x in Type.damage_relations.double_damage_from) {
-          names[count] = Type.damage_relations.double_damage_from[x].name
-          count++
-        }
-      break;
-      case type_effect_enum.HALF_DAMAGE_TO:
-        for (var x in Type.damage_relations.half_damage_to) {
-          names[count] = Type.damage_relations.half_damage_to[x].name
-          count++
-        }
-      break;
-      case type_effect_enum.HALF_DAMAGE_FROM:
-          for (var x in Type.damage_relations.half_damage_from) {
-          names[count] = Type.damage_relations.half_damage_from[x].name
-          count++
-        }
-      break;
-      case type_effect_enum.NO_DAMAGE_TO:
-        for (var x in Type.damage_relations.no_damage_to) {
-          names[count] = Type.damage_relations.no_damage_to[x].name
-          count++
-        }
-      break;
-      case type_effect_enum.NO_DAMAGE_FROM:
-        for (var x in Type.damage_relations.no_damage_from) {
-          names[count] = Type.damage_relations.no_damage_from[x].name
-          count++
-        }
-    }
-  return "Flying"
-    // return (names)
+  var names = [];
+  // console.log(Type_effect)
+  // console.log(Type.damage_relations)
+  // console.log(Type.damage_relations[0])
+  // console.log(Type.damage_relations[0][Type_effect.toString().replace(/ /g, '_')])
+  for (var which in Type.damage_relations[0][Type_effect.toString().replace(/ /g, '_')]) // <-- totally works
+    names.push(which.name) // <-- doesn't append anything
+  return names
 }
