6e3160384bb97e0dcd7e8f775e7e20806ef1e310
Collin Brill <cbrill@e1z4r13p7.42.us.org>
2019-04-12 17:16:05 -0700

    type relations works

--- namespace.pokebix/code/findTypeRelations.js
+++ namespace.pokebix/code/findTypeRelations.js
@@ -1,20 +1,7 @@
-const type_effect_enum = {
-  DOUBLE_DAMAGE_TO: 'double_damage_to',
-  DOUBLE_DAMAGE_FROM: 'double_damage_from',
-  HALF_DAMAGE_TO: 'half_damage_to',
-  HALF_DAMAGE_FROM: 'half_damage_from',
-  NO_DAMAGE_TO: 'no_damage_to',
-  NO_DAMAGE_FROM: 'no_damage_from'
-}
-
-
 module.exports.function = function findTypeRelations(Type, Type_effect) {
   var names = [];
-  // console.log(Type_effect)
-  // console.log(Type.damage_relations)
-  // console.log(Type.damage_relations[0])
-  // console.log(Type.damage_relations[0][Type_effect.toString().replace(/ /g, '_')])
-  for (var which in Type.damage_relations[0][Type_effect.toString().replace(/ /g, '_')]) // <-- totally works
-    names.push(which.name) // <-- doesn't append anything
-  return names
+  var effect_key = Type_effect.toString().replace(/ /g, '_'); // replace with boxby model -> js arr/enum
+  for (var e=0; e<Type.damage_relations[0][effect_key].length; e++)
+    names[e] = Type.damage_relations[0][effect_key][e].name;
+  return names;
 }
