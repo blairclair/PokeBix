8ec627d1d80a716a771a0643aa59d982b639ff48
Ariella Grodzinsky <agrodzin@e1z4r13p11.42.us.org>
2019-04-13 13:46:17 -0700

    layout

--- namespace.pokebix/code/findPokemonSpecies.js
+++ namespace.pokebix/code/findPokemonSpecies.js
@@ -10,8 +10,15 @@ module.exports.function = function findPokemonSpecies (Pokemon) {
   for (var i in flavor){
     if (flavor[i].language.name == "en"){
       Pokemon.flavor_text = flavor[i].flavor_text
+      // console.log(typeof(Pokemon.flavor_text))
+      // console.log(JSON.stringify(Pokemon.flavor_text))
+      Pokemon.flavor_text.replace(/(\r\n|\n|\r)+/gm, ' ').replace(/ +/g, ' ') 
+      break;
     }
     i++;
   }
+   Pokemon.image_set = [
+     {url: "/images/Pokedex.gif"}
+   ]
   return Pokemon
 }
