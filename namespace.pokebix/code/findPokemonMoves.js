4f4e692f9de45d8d38f9bb903b75115d7e0a2f77
Collin Brill <cbrill@student.42.us.org>
2019-04-15 16:25:32 -0700

    no url prop

--- namespace.pokebix/code/findPokemonMoves.js
+++ namespace.pokebix/code/findPokemonMoves.js
@@ -4,10 +4,7 @@ var config = require('config')
 
 module.exports.function = function findPokemonMoves (Pokemon) {
   var track = 0;
-  for (move in Pokemon.moves) {
-    var move = {'name': Pokemon.moves[move].move.name,
-               'url': Pokemon.moves[move].move.url};
-    Pokemon.move_info.push(move);
-  }
+  for (move in Pokemon.moves)
+    Pokemon.move_info.push({'name': Pokemon.moves[move].move.name});
   return Pokemon;
 }
