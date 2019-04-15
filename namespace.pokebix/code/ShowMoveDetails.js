413a4c9aed740302aa0ab93f6d39a8c03cc7bc63
Ariella Grodzinsky <agrodzin@e1z4r13p11.42.us.org>
2019-04-15 16:43:47 -0700

    findMoveByName is the action name

--- namespace.pokebix/code/ShowMoveDetails.js
+++ namespace.pokebix/code/ShowMoveDetails.js
@@ -2,6 +2,5 @@ var http = require('http')
 var console = require('console')
 var config = require('config')
 module.exports.function = function showMoveDetails (Pokemon_move) {
-  console.log(Pokemon_move)
   return Pokemon_move
 }
